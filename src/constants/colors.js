c7bd06dbff3130ed10138da1bf8305c5816ab82b
BL4CKM4G3 <alvaro_rangel@outlook.com>
2021-02-08 18:48:32 -0300

    feat: :sparkles: Improved Color atoms

---
+++ src/constants/colors.js
@@ -0,0 +1,22 @@
+import colors from "vuetify/lib/util/colors";
+
+export default {
+  light: {
+    primary: colors.teal.accent4,
+    secondary: colors.blueGrey.lighten3,
+    accent: colors.lightBlue.accent3,
+    error: colors.red.accent3,
+    info: colors.blue.accent3,
+    success: colors.green.accent3,
+    warning: colors.yellow.accent3
+  },
+  dark: {
+    primary: colors.green.accent4,
+    secondary: colors.blueGrey.darken4,
+    accent: colors.lightBlue.accent4,
+    error: colors.red.accent4,
+    info: colors.blue.accent4,
+    success: colors.green.accent4,
+    warning: colors.yellow.accent4
+  }
+}
