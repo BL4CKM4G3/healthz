41869cd7c4f9f6750991e8ce9edda825d3546ab3
BL4CKM4G3 <alvaro_rangel@outlook.com>
2021-02-08 18:48:53 -0300

    feat: :sparkles: Extracted Colors from Vuetify confs

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -11,26 +11,6 @@ export default new Vuetify({
     options: {
       customProperties: true,
     },
-    themes: {
-      light: {
-        primary: colors.green.accent3,
-        secondary: colors.blueGrey.lighten3,
-        accent: colors.lightBlue.accent3,
-        error: colors.red.accent3,
-        info: colors.blue.accent3,
-        success: colors.green.accent3,
-        warning: colors.yellow.accent3
-      },
-      dark: {
-        primary: colors.green.accent4,
-        secondary: colors.blueGrey.darken4,
-        accent: colors.lightBlue.accent4,
-        error: colors.red.accent4,
-        info: colors.blue.accent4,
-        success: colors.green.accent4,
-        warning: colors.yellow.accent4
-      }
-    },
   },
   lang: {
     locales: { pt },
