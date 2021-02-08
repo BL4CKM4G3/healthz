c3a6cc8ae81a44257eecd44bcfa88a9631098044
BL4CKM4G3 <alvaro_rangel@outlook.com>
2021-02-08 19:33:12 -0300

    feat: :sparkles: Created AppBar organism

---
+++ src/components/AppBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app-bar shaped elevation="10">
+    <v-row align-content="center" class="mt-2 ml-1">
+      <v-text-field
+        color="teal"
+        placeholder="Search your symptoms"
+        flat
+        success
+        :prepend-icon="icons.search"
+      ></v-text-field>
+      <v-spacer></v-spacer>
+      <v-chip class="m-2" color="white">
+        <v-icon left color="teal">
+          {{ icons.clock }}
+        </v-icon>
+        14:02 PM Today Feb,8
+      </v-chip>
+    </v-row>
+  </v-app-bar>
+</template>
+
+<script>
+import * as Icons from "../constants/icons";
+
+export default {
+  name: "AppBar",
+  data: () => ({
+    icons: {
+      search: Icons.ICON_SEARCH,
+      clock: Icons.ICON_TIME,
+    },
+  }),
+};
+</script>
+
+<style>
+</style>
