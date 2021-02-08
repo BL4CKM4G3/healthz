b0c311564407951249f894e83d34fbe0391c24a5
BL4CKM4G3 <alvaro_rangel@outlook.com>
2021-02-08 18:48:18 -0300

    feat: :sparkles: Improved icon atoms

---
+++ src/constants/icons.js
@@ -0,0 +1,9 @@
+import * as mdi from "@mdi/js";
+
+export const ICON_MEDICAL = mdi.mdiMedicalBag;
+export const ICON_DASHBOARD = mdi.mdiViewDashboard;
+export const ICON_ACCOUNT = mdi.mdiAccount;
+export const ICON_MESSAGE = mdi.mdiMessage;
+export const ICON_SETTINGS = mdi.mdiCog;
+export const ICON_HELP = mdi.mdiMessageQuestion;
+export const ICON_LOGOUT = mdi.mdiLogout;
