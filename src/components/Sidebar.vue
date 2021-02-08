22b5107acc1137e9b5bddeec11b75071d8c337f0
BL4CKM4G3 <alvaro_rangel@outlook.com>
2021-02-08 18:15:43 -0300

    feat: :sparkles: Created Sidebar component

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-navigation-drawer
+    v-model="drawer"
+    dark
+    app
+    mini-variant
+    mini-variant-width="100"
+    :class="$vuetify.theme.themes.dark.primary"
+  >
+
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: "Sidebar",
+  data: () => ({
+    drawer: true,
+  })
+}
+</script>
+
+<style>
+
+</style>
