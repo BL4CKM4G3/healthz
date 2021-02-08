afc1ac9fba46deac96b76234b63d2f11d3424d78
BL4CKM4G3 <alvaro_rangel@outlook.com>
2021-02-08 19:34:25 -0300

    feat: :sparkles: Improved app component to render route and transition wrapper

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,11 @@
   <v-app>
     <sidebar></sidebar>
     <v-main>
+      <transition name="fade">
+        <v-container>
+          <router-view></router-view>
+        </v-container>
+      </transition>
     </v-main>
   </v-app>
 </template>
