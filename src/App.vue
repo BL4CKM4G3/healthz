83acd329d4a6c7a0ae77be32fe75583529713f31
BL4CKM4G3 <alvaro_rangel@outlook.com>
2021-02-08 23:45:45 -0300

    feat: :sparkles: Improved main app component scroll bar

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
 <template>
-  <v-app>
-    <sidebar></sidebar>
-    <v-main>
-      <transition name="fade">
-        <v-container>
-          <router-view></router-view>
-        </v-container>
-      </transition>
-    </v-main>
-  </v-app>
+  <div id="app">
+    <v-app>
+      <sidebar></sidebar>
+      <v-main>
+        <transition name="fade">
+          <v-container class="mr-0 pr-0 pl-0 ml-0">
+            <router-view></router-view>
+          </v-container>
+        </transition>
+      </v-main>
+    </v-app>
+  </div>
 </template>
 
 <script>
-import Sidebar from './components/Sidebar.vue';
+import Sidebar from "./components/Sidebar.vue";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
     Sidebar,
@@ -26,3 +28,14 @@ export default {
   }),
 };
 </script>
+
+<style lang="scss">
+html::-webkit-scrollbar {
+  width: 6px;
+  background: #01d3b7;
+}
+html::-webkit-scrollbar-thumb {
+  width: 6px;
+  background: #004D40;
+}
+</style>
